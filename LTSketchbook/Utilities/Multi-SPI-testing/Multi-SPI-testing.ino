61755a46078a7b09177ab436dc3848d8a4619f13
Marc Gonzalez <Marc.Gonzalez@analog.com>
2024-06-28 15:50:07 -0600

    Initial Commit

---
+++ LTSketchbook/Utilities/Multi-SPI-testing/Multi-SPI-testing.ino
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <stdint.h>
+#include <util/delay.h>
+#include "Linduino.h"
+#include "LT_I2C.h"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("***"));
+}
+
+void loop() {
+
+}
